0707e579380f3d4391fad4b0bd2ff10e88c68ddd
bartdazz <bartdazz@gmail.com>
2025-03-29 10:36:52 +0100

    In the G89HW1 file: - Added global variables L, K, M and printed them - Created a key value pairs RDD with point (Vector) and demographic class ("A" or   "B") - Added some lines to check that the code works
    Updated the gitignore and added also an example file named
    test_vectors.txt.
    
    To run the program on Intellij use as input test_vectors.txt 1 1 1
    (the "1 1 1" is used to assign the values of L, K, M)

--- src/G89HW1.java
+++ src/G89HW1.java
@@ -38,30 +38,65 @@ public class G89HW1 {
         // read the text file in input
         JavaRDD<String> points = sc.textFile(args[0]).cache();
         // to the previous line we can eventually add
-        //.repartition(K) 
+        // .repartition(K)
         // to tell spark to divide the input in K (number to be choosen) partitions
         // inside the RDD
 
         int L = Integer.parseInt(args[1]);
         int K = Integer.parseInt(args[2]);
         int M = Integer.parseInt(args[3]);
-        
+
         // print command-line arguments
         System.out.println("The number of partitions is: " + L);
         System.out.println("The number of centroids is: " + K);
         System.out.println("The number of iterations for the LLoyd's algorithm is: " + M);
 
-
-
         // SETTING GLOBAL VARIABLES
         long numpoints = points.count();
         System.out.println("The number of points is " + numpoints);
 
-
         // build a key-value pairs RDD
-        // 
+        JavaPairRDD<Vector, String> inputPoints;
 
+        // the following chunk of code will create a key-value pairs RDD
+        inputPoints = points
+                .flatMapToPair((pointClass) -> {
+                    // splitting the String representing the point and it's class
+                    String[] tokens = pointClass.split(",");
 
+                    // getting the length of the splitted array
+                    Integer len = tokens.length;
+
+                    // Extracting the sublist regarding the point's coordinates
+                    String[] pointString = Arrays.copyOfRange(tokens, 0, len - 1);
+
+                    // Transforming the coordinates into doubles
+                    // COPIED FROM CHATGPT : UNDERSTAND BETTER !!!
+                    double[] pointDouble = Arrays
+                            .stream(pointString)
+                            .mapToDouble(Double::parseDouble)
+                            .toArray();
+
+                    // Creating the vector
+                    Vector point = Vectors.dense(pointDouble);
+
+                    // Extracting the demographic class
+                    String demoClass = tokens[len - 1];
+
+                    // Create the ArrayList that will contain the key-value pairs
+                    ArrayList<Tuple2<Vector, String>> pairs = new ArrayList<>();
+
+                    // Create the Tuple2 of a single pair point-class
+                    Tuple2<Vector, String> singlePair = new Tuple2<Vector, String>(point, demoClass);
+
+                    // Add the pair to the ArrayList pairs
+                    pairs.add(singlePair);
+
+                    // return an iterator over the pairs as the result of this function
+                    return pairs.iterator();
+                });
+        long lenPoints = points.count();
+        System.out.println("The number of points is " + lenPoints);
 
         // prova vettori
         // Create a vector using Vectors.dense
